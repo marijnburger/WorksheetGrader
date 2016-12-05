0f4528faced414b16ca3a48cf16513706874fb6a
marijnburger <marijnburger@Marijns-MacBook-Pro.local>
2016-12-05 00:26:44 -0800

    oops

--- WorksheetGrader/Program4.cpp
+++ WorksheetGrader/Program4.cpp
@@ -4,17 +4,23 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
-#include <string>
 
 using namespace std;
 using namespace cv;
 using namespace cv::text;
 
+// **** Function Declarations **************************************************
+
+
+// **** Main *******************************************************************
 int main()
 {
 	EndToEndWrapper e2e = EndToEndWrapper();
 	e2e.run("test5.JPG");
 	Mat output = imread("recognition.JPG");
+	namedWindow("recognition", WINDOW_NORMAL);
 	imshow("recognition", output);
 	waitKey(0);
 }
+
+// **** Function Definitions ***************************************************
