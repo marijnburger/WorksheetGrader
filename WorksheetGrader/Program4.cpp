bf723d4bc0d3c94b1fa47cc898dbac40b80f0bea
JackScottie <eldridge.jack@gmail.com>
2016-12-05 19:36:30 -0800

    add compareSolutions

--- WorksheetGrader/Program4.cpp
+++ WorksheetGrader/Program4.cpp
@@ -11,7 +11,43 @@ using namespace std;
 using namespace cv;
 using namespace cv::text;
 
+// **** Class Constants ********************************************************
+const int NUMBER_OF_QUESTIONS = 3;
+
 // **** Function Declarations **************************************************
+void assignGrade(String theFilename, float score, float possible);
+
+int compareAnswers(String* answers, String* solutions);
+
+// **** Main *******************************************************************
+int main()
+{
+	/*
+	EndToEndWrapper e2e = EndToEndWrapper();
+	e2e.run("test5.JPG");
+	Mat output = imread("recognition.JPG");
+	namedWindow("recognition", WINDOW_NORMAL);
+	imshow("recognition", output);
+	waitKey(0);
+	*/
+
+	String someAnswers[NUMBER_OF_QUESTIONS] = { "Hotel", "Yes", "false" };
+	String theSolution[NUMBER_OF_QUESTIONS] = { "Hotel", "Yes", "false" };
+
+	float theScore = compareAnswers(someAnswers, theSolution);
+
+	Mat gradedPaper = imread("test5.JPG");
+	imwrite("gradedPaper.JPG", gradedPaper);
+	assignGrade("gradedPaper.JPG", theScore, NUMBER_OF_QUESTIONS);
+
+	Mat aGradedPaper = imread("gradedPaper.JPG");
+	namedWindow("Graded Paper", WINDOW_NORMAL);
+	imshow("Graded Paper", aGradedPaper);
+	waitKey(0);
+
+}
+
+// **** Function Definitions ***************************************************
 
 // Place a grade on an assignment. 
 // Score = points earned
@@ -23,35 +59,23 @@ void assignGrade(String theFilename, float score, float possible)
 	String scoreString = to_string(percent) + "%";
 	Mat theImage = imread(theFilename);
 
-	putText(theImage, scoreString, cvPoint(200,200), FONT_HERSHEY_DUPLEX, 6.0,
-		CV_RGB(255,0,0), 8, CV_AA);
+	putText(theImage, scoreString, cvPoint(200, 200), FONT_HERSHEY_DUPLEX, 6.0,
+		CV_RGB(255, 0, 0), 8, CV_AA);
 
 	imwrite("gradedPaper.JPG", theImage);
-
 }
 
-// **** Main *******************************************************************
-int main()
-{
-	
-	EndToEndWrapper e2e = EndToEndWrapper();
-	e2e.run("test5.JPG");
-	Mat output = imread("recognition.JPG");
-	namedWindow("recognition", WINDOW_NORMAL);
-	imshow("recognition", output);
-	waitKey(0);
-	
-
-	Mat gradedPaper = imread("test5.JPG");
-	imwrite("gradedPaper.JPG", gradedPaper);
-	assignGrade("gradedPaper.JPG", 75, 100);
-
-	
-	Mat aGradedPaper = imread("gradedPaper.JPG");
-	namedWindow("Graded Paper", WINDOW_NORMAL);
-	imshow("Graded Paper", aGradedPaper);
-	waitKey(0);
-	
+// Compare answer to solutions and output
+// number of matches as an int
+int compareAnswers(String* answers, String* solutions)
+{	
+	float count = 0.0;
+	for (int i = 0; i < NUMBER_OF_QUESTIONS; i++)
+	{
+		if (answers[i].compare(solutions[i]) == 0)
+		{
+			count = count + 1;
+		}
+	}
+	return count;
 }
-
-// **** Function Definitions ***************************************************
